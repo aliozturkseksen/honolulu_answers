690950324538fd13b08c5af873c72bdd36dc6b1e
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-01 15:20:57 -0500

    Revert "store the stack name in SDB, not as a parameter."
    This reverts commit 169fe88279d42fbb71aabea7377d85fe38ec2093.

--- infrastructure/build-and-deploy.sh
+++ infrastructure/build-and-deploy.sh
@@ -10,5 +10,4 @@ gem install aws-sdk-core --pre
 export stack_name=HonoluluAnswers-`date +%Y%m%d%H%M%S`
 aws cloudformation create-stack --stack-name $stack_name --template-body "`cat infrastructure/config/honolulu.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM"
 ruby infrastructure/bin/monitor_stack.rb  --stack $stack_name
-
-ruby -e 'require "opendelivery"' -e "OpenDelivery::Domain.new('us-west-2').set_property 'honolulu-jenkins-jonny-test','$pipeline_instance_id', 'stack_name', '$stack_name'"
+echo stack_name=$stack_name > stackname.txt
