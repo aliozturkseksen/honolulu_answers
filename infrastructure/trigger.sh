96ba2137b5e5619fa0fb271a9147262c2fe77fc7
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-01 11:35:46 -0500

    adding trigger script. it will create the pipeline id and store information in sdb.

---
+++ infrastructure/trigger.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+# generate pipeline instance id
+export pipeline_instance_id=`date +%Y%m%d%H%M%S`-$BUILD_NUMBER
+echo pipeline_instance_id=$pipeline_instance_id
+
+# lookup the SHA of the latest commit
+GIT_SHA=`git log | head -1 | awk '{ print $2 }'`
+
+# save instance id to SDB
+ruby -e 'require "opendelivery"' -e "OpenDelivery::Domain.new('us-west-2').set_property 'honolulu-jenkins-jonny-test','$pipeline_instance_id', 'SHA', '$GIT_SHA'"
+
+# push instance id into file so we can load it into the environment
+echo pipeline_instance_id=$pipeline_instance_id > environment.txt
