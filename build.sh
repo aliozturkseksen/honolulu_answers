5a0253b956f695d4121b7f61577a5f0c51ea2f8a
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-01 15:20:59 -0500

    Revert "checkout the same revision that triggered the pipeline when doing the commit stage."
    This reverts commit 3b7ed66419972aa67c9076cfef9258c31098b3af.

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -e
 
-export SHA=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('us-west-2').get_property 'honolulu-jenkins-jonny-test','$pipeline_instance_id', 'SHA'"`
-echo checking out revision $SHA
-git checkout $SHA
-
 export RAILS_ENV="test"
 export BHT_API_KEY="7868d47a7c43908cc80a44738acebb41"
 
