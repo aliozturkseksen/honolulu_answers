f48c58053ef640da20339a892ccee0c4247de2b3
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-01 15:32:27 -0500

    replaying all my changes on a branch. This commit makes me look super productive

--- infrastructure/test-application.sh
+++ infrastructure/test-application.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -e
-gem install cucumber net-ssh
+gem install cucumber net-ssh opendelivery
+
+export SHA=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('us-west-2').get_property 'honolulu-jenkins-jonny-test','$pipeline_instance_id', 'SHA'"`
+echo checking out revision $SHA
+git checkout $SHA
+
+export stack_name=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('us-west-2').get_property 'honolulu-jenkins-jonny-test','$pipeline_instance_id', 'stack_name'"`
+
 cd infrastructure
 cucumber
