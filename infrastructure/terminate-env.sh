bf931f00c94e01a1fc793c93bbcaddeb201abff8
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-01 12:11:41 -0500

    get the SHA and stack name from SDB, and then do run the tests against that revision and stack name

--- infrastructure/terminate-env.sh
+++ infrastructure/terminate-env.sh
@@ -1,2 +1,8 @@
 #!/bin/bash -e
+export SHA=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('us-west-2').get_property 'honolulu-jenkins-jonny-test','$pipeline_instance_id', 'SHA'"`
+echo checking out revision $SHA
+git checkout $SHA
+
+export stack_name=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('us-west-2').get_property 'honolulu-jenkins-jonny-test','$pipeline_instance_id', 'stack_name'"`
+
 aws cloudformation delete-stack --stack-name $stack_name --region $region
