970ddc5b90ad96bc043df51179570374e67cfacf
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-08 14:51:57 -0500

    lookup the production stack name to test against.

--- infrastructure/smoke-test.sh
+++ infrastructure/smoke-test.sh
@@ -5,7 +5,7 @@ export SHA=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('
 echo checking out revision $SHA
 git checkout $SHA
 
-export stack_name=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('$region').get_property '$sdb_domain','$pipeline_instance_id', 'stack_name'"`
+export stack_name=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('$region').get_property '$sdb_domain','$pipeline_instance_id', 'production_stack_name'"`
 
 cd infrastructure
 cucumber
