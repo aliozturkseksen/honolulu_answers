24ab46ca17b2bb796a0f6cfd6a0a66979fa453bc
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-08 15:01:11 -0500

    put production in the stack name for production stacks.

--- infrastructure/build-and-deploy-for-prod.sh
+++ infrastructure/build-and-deploy-for-prod.sh
@@ -6,7 +6,7 @@ git checkout $SHA
 
 gem install trollop opendelivery
 gem install aws-sdk-core --pre
-export stack_name=HonoluluAnswers-`date +%Y%m%d%H%M%S`
+export stack_name=HonoluluAnswers-Production-`date +%Y%m%d%H%M%S`
 aws cloudformation create-stack --stack-name $stack_name --template-body "`cat infrastructure/config/honolulu.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM"
 ruby infrastructure/bin/monitor_stack.rb  --stack $stack_name
 
