be44d2bb61bad91c811e7d089fdbb4f43dd6ab87
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-08 18:42:38 -0500

    use the started_at info for the timestamp, so that itemName() will match up with the stack name.

--- infrastructure/build-and-deploy.sh
+++ infrastructure/build-and-deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 
 export SHA=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('$region').get_property '$sdb_domain','$pipeline_instance_id', 'SHA'"`
+export timestamp=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('$region').get_property '$sdb_domain','$pipeline_instance_id', 'started_at'"`
 echo checking out revision $SHA
 git checkout $SHA
 
 
 gem install trollop opendelivery
 gem install aws-sdk-core --pre
-export stack_name=HonoluluAnswers-`date +%Y%m%d%H%M%S`
+export stack_name=HonoluluAnswers-$timestamp
 aws cloudformation create-stack --stack-name $stack_name --template-body "`cat infrastructure/config/honolulu.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM"
 ruby infrastructure/bin/monitor_stack.rb  --stack $stack_name
 
