2ce1f1659e97e28eb29ed85c20813ab866f89f5e
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-09 19:00:31 -0500

    apparently if an exception is thrown in ruby code as part as a -e block, it doesn't cause ruby to exit with an error code. faking it.

--- infrastructure/production-trigger.sh
+++ infrastructure/production-trigger.sh
@@ -8,7 +8,10 @@ export query="select * from \`honolulu-jenkins-jonny-test\` where furthest_pipel
 
 export pipeline_instance_id=`ruby -e 'require "aws-sdk-core"' -e "puts Aws::SDB.new(region: '$region').select(select_expression: '$query').items.first.name"`
 
-export SHA=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('$region').get_property '$sdb_domain','$pipeline_instance_id', 'SHA'"`
+if [[ -z "$pipeline_instance_id" ]]
+    echo "failed to retrieve info from configuration store."
+    exit 1
+fi
 
 # push instance id into file so we can load it into the environment
 echo pipeline_instance_id=$pipeline_instance_id > environment.txt
