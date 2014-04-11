9ad831e0f94a704a63d11189834952b3db3709b7
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-11 15:36:49 -0500

    install opendelivery so we can use it.

--- infrastructure/bluegreen.sh
+++ infrastructure/bluegreen.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+gem install opendelivery
+
 export SHA=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('$region').get_property '$sdb_domain','$pipeline_instance_id', 'SHA'"`
 echo checking out revision $SHA
 git checkout $SHA
