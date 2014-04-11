b06fcfe6c1518dcc2e0c934dd08c50a0eada3630
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-11 15:10:22 -0500

    got the script to actually. you know. work.

--- infrastructure/bin/route53switch.rb
+++ infrastructure/bin/route53switch.rb
@@ -9,22 +9,53 @@ opts = Trollop::options do
   opt :stackname, "The name of the CloudFormation stack that is intended to become the new production stack", :type => String, :required => true
 end
 
-#route 53 and opsworks only run in us-east-1
-r53 = Aws::Route53.new(region: 'us-east-1')
-ops = Aws::OpsWorks.new(region: "us-east-1")
+def get_zone_id r53, hosted_zone_name
+  zone_id = r53.list_hosted_zones.hosted_zones.collect {|zone| if zone.name == "#{hosted_zone_name}." then zone.id.split('/').last end }.compact.first
+  return zone_id
+end
 
-# cfn needs to be in the same 
+def get_old_ip_address r53, subdomain, hosted_zone_name
+# Get the IP Address of the existing Production Instance from Route 53 itself.
+   zone_id = get_zone_id r53, hosted_zone_name
+   resource_record = r53.list_resource_record_sets(hosted_zone_id: zone_id).resource_record_sets.select {|record| record.name == "#{subdomain}.#{hosted_zone_name}."}
+   if (resource_record.nil? || resource_record.empty?) then raise "No resource record exists for #{subdomain}.#{hosted_zone_name}." end
+   old_ip_address = resource_record.first.resource_records.first.value
+   return old_ip_address
+end
+
+def get_new_ip_address cfn, ops, stack_name
+  instance_id = get_opsworks_instance_id cfn, stack_name
+  # Get the IP Address from the OpsWorks instance information
+  new_ip_address = ops.describe_instances(instance_ids: [instance_id]).first.instances.first.public_ip
+  return new_ip_address
+end
+
+def get_opsworks_instance_id cfn, stack_name
+  # get the OpsWorks Instance ID from the CloudFormation stack
+  instance_id = nil
+  cfn.describe_stack_resources(stack_name: stack_name).stack_resources.each do |resource|
+    if resource.resource_type == "AWS::OpsWorks::Instance"
+      instance_id = cfn.describe_stack_resource(stack_name: stack_name, logical_resource_id: resource.logical_resource_id).stack_resource_detail.physical_resource_id
+    end
+  end
+  return instance_id
+end
+
+#route 53 and opsworks only run in us-east-1
+r53 = Aws::Route53.new(region: "us-east-1")
+ops = Aws::OpsWorks.new(region: "us-east-1")
+# cfn needs to be in the same region as everything else, tho.
 cfn = Aws::CloudFormation.new(region: opts[:region])
 
-new_ip_address = get_new_ip_address cfn, opts[:stackname]
+new_ip_address = get_new_ip_address cfn, ops, opts[:stackname]
 old_ip_address = get_old_ip_address r53, opts[:subdomain], opts[:hostedzone]
 
 # Tell Route 53 to switch the new IP in for the old one
 full_address = "#{opts[:subdomain]}.#{opts[:hostedzone]}"
-r53.change_resource_record_sets hosted_zone_id: hosted_zone.id.split('/')[2], change_batch: 
+zone_id = get_zone_id r53, opts[:hostedzone]
+r53.change_resource_record_sets hosted_zone_id: zone_id, change_batch: 
 { 
-  changes: 
-  [
+  changes: [
     {
       action: "DELETE", 
       resource_record_set: {
@@ -44,28 +75,3 @@ r53.change_resource_record_sets hosted_zone_id: hosted_zone.id.split('/')[2], ch
     }
   ] 
 }
-
-
-def get_old_ip_address r53, subdomain, hosted_zone_name
-# Get the IP Address of the existing Production Instance from Route 53 itself.
-  zone_id = r53.list_hosted_zones.hosted_zones.collect {|zone| if zone.name == "#{hosted_zone_name}." then zone.id.split('/').last end }.compact.first
-  old_ip_address = r53.list_resource_record_sets(hosted_zone_id: zone_id).resource_record_sets.select {|record| record.name == "#{subdomain}.#{hosted_zone_name}."}.first.resource_records.first.value
-end
-
-def get_new_ip_address cfn, stack_name
-  instance_id = get_opsworks_instance_id cfn, stack_name
-  # Get the IP Address from the OpsWorks instance information
-  new_ip_address = ops.describe_instances(instance_ids: [instance_id]).first.instances.first.public_ip
-  return new_ip_address
-end
-
-def get_opsworks_instance_id cfn, stack_name
-  # get the OpsWorks Instance ID from the CloudFormation stack
-  instance_id = nil
-  cfn.describe_stack_resources(stack_name: stack_name).stack_resources.each do |resource|
-    if resource.resource_type == "AWS::OpsWorks::Instance"
-      instance_id = cfn.describe_stack_resource(stack_name: stack_name, logical_resource_id: resource.logical_resource_id).stack_resource_detail.physical_resource_id
-    end
-  end
-  return instance_id
-end
