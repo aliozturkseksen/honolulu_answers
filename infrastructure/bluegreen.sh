f8780a1b50d2371cb40fa7c70105caa7402a1863
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-13 14:01:37 -0500

    parameterized the hostedzone name. instead of being hardcoded, it's an environment variable, set at the jenkins level. We'll likely want to do this differently in the future.

--- infrastructure/bluegreen.sh
+++ infrastructure/bluegreen.sh
@@ -8,6 +8,6 @@ git checkout $SHA
 
 export production_stack_name=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('$region').get_property '$sdb_domain','$pipeline_instance_id', 'production_stack_name'"`
 
-ruby infrastructure/bin/route53switch.rb  --subdomain honolulu --hostedzone elasticoperations.com --region $region --stackname $production_stack_name
+ruby infrastructure/bin/route53switch.rb  --subdomain honolulu --hostedzone $domain --region $region --stackname $production_stack_name
 ruby -e 'require "opendelivery"' -e "OpenDelivery::Domain.new('$region').set_property '$sdb_domain','$pipeline_instance_id', 'furthest_pipeline_stage_completed', 'bluegreen'"
 
