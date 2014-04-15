072dc7354248cbf30e1ccfe536bd53a08fb4770c
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-15 18:16:42 -0500

    install gems required for running the script first.

--- infrastructure/become-production-jenkins.sh
+++ infrastructure/become-production-jenkins.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+gem install trollop
+gem install aws-sdk-core --pre
+
 echo "making $stack_name the new production Jenkins..."
 
 ruby infrastructure/bin/route53switch.rb  --subdomain pipelinedemo --hostedzone $domain --region $region --stackname $stack_name
