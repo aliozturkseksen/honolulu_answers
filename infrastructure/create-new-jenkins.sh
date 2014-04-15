112ba19cfec0d308faca1493c7fef27fcedf3599
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-15 15:34:00 -0500

    created script for creating new jenkins server.

---
+++ infrastructure/create-new-jenkins.sh
@@ -0,0 +1,6 @@
+export stack_name="Honolulu-Jenkins-`date +%Y%m%d%H%M%`"
+
+https://raw.githubusercontent.com/stelligent/honolulu_jenkins_cookbooks/master/jenkins.template
+
+aws cloudformation create-stack --stack-name $stack_name --template-body "`cat jenkins.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM"
+ruby infrastructure/bin/monitor_stack.rb  --stack $stack_name
