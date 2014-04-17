e4c49b472736385ad685e90bfe876fcbd2bc74c9
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-16 21:37:11 -0500

    pass in the parameter for the domain. just use the same one that the current jenkins has.

--- infrastructure/create-new-jenkins.sh
+++ infrastructure/create-new-jenkins.sh
@@ -4,7 +4,10 @@ export stack_name="Honolulu-Jenkins-`date +%Y%m%d%H%M%S`"
 
 wget https://raw.githubusercontent.com/stelligent/honolulu_jenkins_cookbooks/master/jenkins.template
 
-aws cloudformation create-stack --stack-name $stack_name --template-body "`cat jenkins.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM"
+aws cloudformation create-stack --stack-name $stack_name --template-body "`cat jenkins.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM" --parameters ParameterKey=domain,ParameterValue="${domain}"
+
 ruby infrastructure/bin/monitor_stack.rb  --stack $stack_name
+
+
 
 echo "$stack_name created."
