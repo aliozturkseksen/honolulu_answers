a8bc50179e4ee4a66fa730186e921328ac2da465
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-16 20:42:40 -0500

    added a wait before kicking off the monitor script. AWS sometimes needs a moment to get everything sorted out.

--- infrastructure/build-and-deploy.sh
+++ infrastructure/build-and-deploy.sh
@@ -11,6 +11,8 @@ gem install aws-sdk-core --pre
 export stack_name=HonoluluAnswers-$timestamp
 ruby -e 'require "opendelivery"' -e "OpenDelivery::Domain.new('$region').set_property '$sdb_domain','$pipeline_instance_id', 'stack_name', '$stack_name'"
 aws cloudformation create-stack --stack-name $stack_name --template-body "`cat infrastructure/config/honolulu.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM"
+# make sure we give AWS a chance to actually create the stack...
+sleep 30
 ruby infrastructure/bin/monitor_stack.rb  --stack $stack_name
 
 ruby -e 'require "opendelivery"' -e "OpenDelivery::Domain.new('$region').set_property '$sdb_domain','$pipeline_instance_id', 'furthest_pipeline_stage_completed', 'build-and-deploy'"
