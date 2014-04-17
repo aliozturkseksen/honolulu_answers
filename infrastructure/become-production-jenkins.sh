b331f4806a326ff472a1e18793c774723951ac2c
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-16 22:19:08 -0500

    pull the stack name from the environment. no parameters needed

--- infrastructure/become-production-jenkins.sh
+++ infrastructure/become-production-jenkins.sh
@@ -5,4 +5,4 @@ gem install aws-sdk-core --pre
 
 echo "making $stack_name the new production Jenkins..."
 
-ruby infrastructure/bin/route53switch.rb  --subdomain pipelinedemo --hostedzone $domain --region $region --stackname $stack_name
+ruby infrastructure/bin/route53switch.rb  --subdomain pipelinedemo --hostedzone $domain --region $region --stackname $name_of_jenkins_stack
