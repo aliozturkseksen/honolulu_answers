3544ebf6eb2b36b38a4e1fffc90813a0bda41c64
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-24 14:44:57 -0500

    Added my email as the admin email address.

--- infrastructure/create-new-jenkins.sh
+++ infrastructure/create-new-jenkins.sh
@@ -4,7 +4,7 @@ export stack_name="Honolulu-Jenkins-`date +%Y%m%d%H%M%S`"
 
 wget https://raw.githubusercontent.com/stelligent/honolulu_jenkins_cookbooks/master/jenkins.template
 
-aws cloudformation create-stack --stack-name $stack_name --template-body "`cat jenkins.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM" --parameters ParameterKey=domain,ParameterValue="${domain}"
+aws cloudformation create-stack --stack-name $stack_name --template-body "`cat jenkins.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM" --parameters ParameterKey=domain,ParameterValue="${domain}" ParameterKey=adminEmailAddress,ParameterValue="jonny@stelligent.com"
 
 ruby infrastructure/bin/monitor_stack.rb  --stack $stack_name --region ${region}
 
