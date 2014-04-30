19cd4afd8a6691722b2f0dc9c256d28ff4ebcc1b
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-30 15:54:32 -0500

    implemented stepdefs. let's see if they work? :suspect:

--- pipeline/features/stepdefs/jobs.rb
+++ pipeline/features/stepdefs/jobs.rb
@@ -1,21 +1,36 @@
 require 'aws-sdk-core'
 
-When(/^I inspect the config for "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I inspect the config for "(.*?)"$/) do |job|
+  output_lines = run_cmd.run "cat /var/lib/jenkins/jobs/#{job}.xml"
+
+  @xml_doc = Nokogiri::XML(output_lines)
+
 end
 
 Then(/^I should see multiscm configured for that job$/) do
-  pending # express the regexp above with the code you wish you had
+  nodes = @xml_doc.xpath("//scm[@class='org.jenkinsci.plugins.multiplescms.MultiSCM']")
+  nodes.size.should == 1
+  nodes = @xml_doc.xpath("//scm/scms/scm[@class='hudson.plugins.git.GitSCM']")
+  nodes.size.should == 2
+  nodes = @xml_doc.xpath("//scm/scms/scm/userRemoteConfigs/hudson.plugins.git.UserRemoteConfig/url")
+  nodes.size.should == 2
 end
 
 Then(/^I should see emails turned on for that job$/) do
-  pending # express the regexp above with the code you wish you had
+  nodes = @xml_doc.xpath("//publishers/hudson.plugins.emailext.ExtendedEmailPublisher")
+  nodes.size.should == 1
+  nodes = @xml_doc.xpath("//publishers/hudson.plugins.emailext.ExtendedEmailPublisher/configuredTriggers/hudson.plugins.emailext.plugins.trigger.FailureTrigger")
+  nodes.size.should == 1
+  nodes = @xml_doc.xpath("//publishers/hudson.plugins.emailext.ExtendedEmailPublisher/configuredTriggers/hudson.plugins.emailext.plugins.trigger.FixedTrigger")
+  nodes.size.should == 1
 end
 
 Then(/^I should see that the build step is run in an RVM managed environment$/) do
-  pending # express the regexp above with the code you wish you had
+  nodes = @xml_doc.xpath("//buildWrappers/ruby-proxy-object/ruby-object/object/impl[@pluginid='rvm']")
+  nodes.size.should == 1
 end
 
 Then(/^I should see that each job has Delivery Pipeline configuration$/) do
-  pending # express the regexp above with the code you wish you had
+  nodes = @xml_doc.xpath("//buildWrappers/ruby-proxy-object/ruby-object/object/impl[@pluginid='rvm']")
+  nodes.size.should == 1
 end
