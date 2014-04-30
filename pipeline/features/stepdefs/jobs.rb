42132d360ec3d790c2c6b6ff1d147bc549cd1267
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-30 15:22:26 -0500

    removing other duplicate defs

--- pipeline/features/stepdefs/jobs.rb
+++ pipeline/features/stepdefs/jobs.rb
@@ -1,13 +1,5 @@
 require 'aws-sdk-core'
 
-When(/^I run "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I should see "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
 When(/^I inspect the config for "(.*?)"$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
