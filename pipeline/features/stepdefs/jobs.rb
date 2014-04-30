15eb19d4a30998ead0f0b701bf00353e0ef99291
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-30 15:15:46 -0500

    removing double definition

--- pipeline/features/stepdefs/jobs.rb
+++ pipeline/features/stepdefs/jobs.rb
@@ -1,9 +1,5 @@
 require 'aws-sdk-core'
 
-Given(/^I am testing the local environment$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
 When(/^I run "(.*?)"$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
