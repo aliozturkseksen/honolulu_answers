ee3fa8267456f8dfeac5030dc3555f85435cf969
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-30 15:09:48 -0500

    stepdefs (all pending at the moment)

---
+++ pipeline/features/stepdefs/jobs.rb
@@ -0,0 +1,31 @@
+Given(/^I am testing the local environment$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I run "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I inspect the config for "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see multiscm configured for that job$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see emails turned on for that job$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see that the build step is run in an RVM managed environment$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see that each job has Delivery Pipeline configuration$/) do
+  pending # express the regexp above with the code you wish you had
+end
