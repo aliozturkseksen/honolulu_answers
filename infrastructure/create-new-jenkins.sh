239f1bc7d455b2b208e52b543ef002e389057c41
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-05-12 09:33:11 -0500

    install necessary gems

--- infrastructure/create-new-jenkins.sh
+++ infrastructure/create-new-jenkins.sh
@@ -2,6 +2,9 @@
 
 export stack_name="Honolulu-Jenkins-`date +%Y%m%d%H%M%S`"
 
+gem install trollop
+gem install aws-sdk-core --pre
+
 wget https://raw.githubusercontent.com/stelligent/honolulu_jenkins_cookbooks/master/jenkins.template
 
 aws cloudformation create-stack --stack-name $stack_name --template-body "`cat jenkins.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM" --parameters ParameterKey=domain,ParameterValue="${domain}" ParameterKey=adminEmailAddress,ParameterValue="jonny@stelligent.com"
