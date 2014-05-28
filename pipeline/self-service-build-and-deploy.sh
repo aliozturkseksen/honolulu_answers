b519750f624d0486568af7ec3a5d6fa778eadc4f
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-05-28 14:52:40 -0500

    script for self-service job

---
+++ pipeline/self-service-build-and-deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+echo checking out revision $SHA
+git checkout $SHA
+
+
+gem install trollop opendelivery --no-ri --no-rdoc
+gem install aws-sdk-core --pre --no-ri --no-rdoc
+export stack_name=HonoluluAnswers-$timestamp
+aws cloudformation create-stack --stack-name $stack_name --template-body "`cat infrastructure/config/honolulu.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM"
+# make sure we give AWS a chance to actually create the stack...
+sleep 30
+ruby infrastructure/bin/monitor_stack.rb  --stack $stack_name --region ${region}
+
