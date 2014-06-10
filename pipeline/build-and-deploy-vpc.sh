60634493f899298edd3016e65d5121fd20d5e794
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-06-10 14:04:31 -0500

    lets see if we can get this to run as part of the pipeline

--- pipeline/build-and-deploy-vpc.sh
+++ pipeline/build-and-deploy-vpc.sh
@@ -5,12 +5,21 @@ export timestamp=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain
 echo checking out revision $SHA
 git checkout $SHA
 
+# hardcoding this for now until I figure out how to get it into Jenkins
+VPC=vpc-35947a50
+PublicSubnet=subnet-7300e204
+PrivateSubnetA=subnet-7200e205
+PrivateSubnetB=subnet-4fae682a
 
 gem install trollop opendelivery --no-ri --no-rdoc
 gem install aws-sdk-core --pre --no-ri --no-rdoc
 export stack_name=HonoluluAnswers-$timestamp
 ruby -e 'require "opendelivery"' -e "OpenDelivery::Domain.new('$region').set_property '$sdb_domain','$pipeline_instance_id', 'stack_name', '$stack_name'"
-aws cloudformation create-stack --stack-name $stack_name --template-body "`cat pipeline/config/honolulu.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM" ParameterKey=vpc,ParameterValue=vpc-a5ab45c0 ParameterKey=publicSubnet,ParameterValue="subnet-30d2d276" ParameterKey=privateSubnet,ParameterValue="subnet-33d2d275"
+aws cloudformation create-stack --stack-name $stack_name --template-body "`cat pipeline/config/honolulu.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM" \
+  ParameterKey=vpc,ParameterValue=$VPC \
+  ParameterKey=publicSubnet,ParameterValue=$PublicSubnet \
+  ParameterKey=privateSubnetA,ParameterValue=$PrivateSubnetA \
+  ParameterKey=privateSubnetB,ParameterValue=$PrivateSubnetB \
 
 # make sure we give AWS a chance to actually create the stack...
 sleep 30
