f8f741423c9f6c007d0f7fbea7c10d748f0b5206
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-06-10 16:43:19 -0500

    fixing case

--- pipeline/build-and-deploy-vpc.sh
+++ pipeline/build-and-deploy-vpc.sh
@@ -5,16 +5,18 @@ export timestamp=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain
 echo checking out revision $SHA
 git checkout $SHA
 
+
+
 gem install trollop opendelivery --no-ri --no-rdoc
 gem install aws-sdk-core --pre --no-ri --no-rdoc
 export stack_name=HonoluluAnswers-$timestamp
 ruby -e 'require "opendelivery"' -e "OpenDelivery::Domain.new('$region').set_property '$sdb_domain','$pipeline_instance_id', 'stack_name', '$stack_name'"
 aws cloudformation create-stack --stack-name $stack_name --template-body "`cat pipeline/config/honolulu.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM" \
 --parameters \
-  ParameterKey=vpc,ParameterValue=$VPC \
-  ParameterKey=publicSubnet,ParameterValue=$PublicSubnet \
-  ParameterKey=privateSubnetA,ParameterValue=$PrivateSubnetA \
-  ParameterKey=privateSubnetB,ParameterValue=$PrivateSubnetB \
+  ParameterKey=vpc,ParameterValue=$vpc \
+  ParameterKey=publicSubnet,ParameterValue=$publicSubnet \
+  ParameterKey=privateSubnetA,ParameterValue=$privateSubnetA \
+  ParameterKey=privateSubnetB,ParameterValue=$privateSubnetB \
 
 # make sure we give AWS a chance to actually create the stack...
 sleep 30
