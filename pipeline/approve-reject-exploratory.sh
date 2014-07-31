a56d5c4e5a5f1a4dea4a25002e6df2807d65c09b
Brian Jakovich <brian.jakovich@stelligent.com>
2014-07-31 10:26:12 -0400

    Added exploratory and preprod gate checks/controls

--- pipeline/approve-reject-exploratory.sh
+++ pipeline/approve-reject-exploratory.sh
@@ -1,1 +1,18 @@
-# empty
+#!/bin/bash -e
+
+export SHA=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('$region').get_property '$sdb_domain','$pipeline_instance_id', 'SHA'"`
+
+ruby pipeline/bin/emails/exploratory_check_email.rb \
+--region $region \
+--pipelineid $pipeline_instance_id \
+--recipient tech@stelligent.com \
+--sender tech@stelligent.com \
+--jenkinsurl jenkins.$domain \
+--application "Honolulu" \
+--gitsha $SHA
+
+ruby pipeline/bin/gate_check.rb \
+--region $region \
+--sdbdomain $sdb_domain \
+--pipelineid $pipeline_instance_id \
+--check "exploratory_check"
--- pipeline/approve-reject-preprod.sh
+++ pipeline/approve-reject-preprod.sh
@@ -1,1 +1,15 @@
-# empty
+#!/bin/bash -e
+
+ruby .pipeline/bin/emails/gate_check_email.rb \
+--region $region \
+--pipelineid $pipeline_instance_id \
+--recipient tech@stelligent.com \
+--sender tech@stelligent.com \
+--jenkinsurl jenkins.$domain \
+--application "Honolulu"
+
+ruby .pipeline/bin/gate_check.rb \
+--region $region \
+--sdbdomain $sdb_domain \
+--pipelineid $pipeline_instance_id \
+--check "preprod_check"
