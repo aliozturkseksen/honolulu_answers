f4a34157552729e109c2cdf39f3175860293927b
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-08-14 19:27:55 -0500

    don't need the . there.

--- pipeline/approve-reject-preprod.sh
+++ pipeline/approve-reject-preprod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-ruby .pipeline/bin/emails/preprod_check_email.rb \
+ruby pipeline/bin/emails/preprod_check_email.rb \
 --region "us-east-1" \
 --pipelineid $pipeline_instance_id \
 --recipient jonny@stelligent.co \
@@ -8,7 +8,7 @@ ruby .pipeline/bin/emails/preprod_check_email.rb \
 --jenkinsurl samplepipeline.$domain \
 --application "Honolulu"
 
-ruby .pipeline/bin/gate_check.rb \
+ruby pipeline/bin/gate_check.rb \
 --region $region \
 --sdbdomain $sdb_domain \
 --pipelineid $pipeline_instance_id \
