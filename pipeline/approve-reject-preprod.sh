fe35f73ba57632404d75b9eb14449868e582618a
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-08-14 19:30:00 -0500

    use correct email, pass in sha

--- pipeline/approve-reject-preprod.sh
+++ pipeline/approve-reject-preprod.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -e
 
+export SHA=`ruby -e 'require "opendelivery"' -e "puts OpenDelivery::Domain.new('$region').get_property '$sdb_domain','$pipeline_instance_id', 'SHA'"`
+
 ruby pipeline/bin/emails/preprod_check_email.rb \
 --region "us-east-1" \
 --pipelineid $pipeline_instance_id \
---recipient jonny@stelligent.co \
---sender jonny@stelligent.co \
+--recipient jonny@stelligent.com \
+--sender jonny@stelligent.com \
 --jenkinsurl samplepipeline.$domain \
 --application "Honolulu"
+--gitsha $SHA
 
 ruby pipeline/bin/gate_check.rb \
 --region $region \
