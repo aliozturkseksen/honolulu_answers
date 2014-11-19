96aba48aefb1f432f390a0ac3cb1a4fa21f8cbc2
Paul Duvall <paul.duvall@stelligent.com>
2014-11-19 00:18:44 -0500

    Update approve-reject-preprod.sh
    Wow! This is painful. Changed to paul email for this shell script

--- pipeline/approve-reject-preprod.sh
+++ pipeline/approve-reject-preprod.sh
@@ -6,7 +6,7 @@ ruby pipeline/bin/emails/preprod_check_email.rb \
 --region "us-east-1" \
 --pipelineid $pipeline_instance_id \
 --recipient jonny@stelligent.com \
---sender jonny@stelligent.com \
+--sender paul.duvall@stelligent.com \
 --jenkinsurl samplepipeline.$domain \
 --application "Honolulu" \
 --gitsha $SHA
